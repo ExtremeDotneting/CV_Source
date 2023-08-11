ca03d9d9aaec6078bb3a4fbd52c32cb6dc76c82c
Anonymous <>
2023-08-11 12:17:49 +0300

    С

--- scripts/deploy to github pages.bat
+++ scripts/deploy to github pages.bat
@@ -1,3 +1,4 @@
 cd ..
-git subtree push --prefix deployDist origin gh-pages
+git subtree push --prefix deployDist CV_Source gh-pages 
+git push origin `git subtree split --prefix dist master`:gh-pages --force
 pause
